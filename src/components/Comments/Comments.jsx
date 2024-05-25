73d96a8aec646ce56437a08272cd92b396202ad3
eriksgda <erik.ksgda@gmail.com>
2024-05-24 21:43:41 -0300

    update like count function

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -12,7 +12,9 @@ export function Comments({content, onDeleteComment}) {
   }
 
   function handleLikeComment() {
-    setLikeCount(likeCount + 1);
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
